9abdb69bb4d150a85608de3e0773e93cac6499b0
Spacecowboi <cunningham2192@gmail.com>
2023-08-24 18:34:42 -0700

    almost finished

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const axios = require('axios');
 const port = 3001; 
 const W_API_KEY=process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY=process.env.MOVIE_API_KEY;
 
 class Forecast {
   constructor(date, description) {
@@ -41,6 +42,33 @@ app.get('/weather', async (req, res) => {
   console.error(error);
   res.status(500).json({ error: 'Internal server error. Dont worry its not you.'});
 }
+});
+
+app.get('/movies', async (req, res) => {
+  const { city } = req.query;
+
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}`);
+    const movies = response.data.results;
+
+    const movieList = movies.map((movie) => {
+      const { title, overview, vote_average, vote_count, poster_path, popularity, release_date } = movie;
+      return {
+        title,
+        overview,
+        average_votes: vote_average,
+        total_votes: vote_count,
+        image_url: `https://image.tmdb.org/t/p/w500${poster_path}`,
+        popularity,
+        released_on: release_date
+      };
+    });
+
+    res.json(movieList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.listen(port, () => {
